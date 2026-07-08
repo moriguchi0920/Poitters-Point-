1ede423cb35377f683f30706f1775eb20be03cb8
DESKTOP-HG7MJFF\森口皓都 <oca2502sg0023@edu.oca.ac.jp>
2026-07-08 20:47:26 +0900

    岩の仮素材追加

---
+++ BaseProject2026_0613/src/Game/Object/PoittersPoint_Rock.cpp
@@ -0,0 +1,73 @@
+﻿//---------------------------------------------------------------------------
+//! @file   PoittersPoint_Rock.cpp
+//! @brief  PoittersPoint_Rock
+//---------------------------------------------------------------------------
+#include "PoittersPoint_Rock.h"
+#include "Game/Scene/PoittersPoint_Stage.h"
+
+#include <System/Scene.h>
+#include <System/Component/ComponentModel.h>
+#include"Game/Component/ComponentGrabbable.h"
+
+namespace PoittersPoint {
+// namespace PoittersPoint
+
+bool Rock::Init()
+{
+    // 親(継承元の基底クラス)のInit関数を呼ぶ
+    // これがなければabort()が呼ばれる
+    Super::Init();
+    //__super::Init();
+
+    SetTranslate({0, 5, 0});
+
+    SetName("Rock");
+    AddComponent<ComponentModel>("data/Game/Models/Rock/rock.mv1");
+
+    auto col = AddComponent<ComponentCollisionSphere>()->SetRadius(3.0f);
+
+    col->SetCollisionGroup(ComponentCollision::CollisionGroup::ETC);
+    col->SetHitCollisionGroup((u32)ComponentCollision::CollisionGroup::ENEMY | (u32)ComponentCollision::CollisionGroup::GROUND |
+                              (u32)ComponentCollision::CollisionGroup::ITEM);
+    AddComponent<ComponentGrabbable>();
+
+    //AddComponent<ComponentCollisionModel>();
+    //if(auto collision = GetComponent<ComponentCollisionModel>()) {
+    //    collision->AttachToModel();    // コリジョンをモデルに合わせる
+    //}
+
+
+
+    return true;
+}
+
+void Rock::Update()
+{
+    __super::Update();
+
+}
+
+void Rock::GUI()
+{
+    __super::GUI();
+}
+
+
+
+void Rock::OnHit(const ComponentCollision::HitInfo& hit_info)
+{
+    /*
+    // 当たった相手の名前がEnemyだったら消去する
+    auto name = hit_info.hit_collision_->GetOwner()->GetNameDefault();
+    if(name == "Enemy") {
+        hit_info.collision_->SetCollisionStatus(ComponentCollision::CollisionBit::DisableHit, true);
+        // 自分を削除する
+        Scene::Object::Release(SharedThis());
+    }
+    */
+
+    // 最後にこれを入れてください。ここでめりこみの解消などの処理を行っています。
+    Super::OnHit(hit_info);
+}
+
+}    // namespace PoittersPoint
---
+++ BaseProject2026_0613/src/Game/Object/PoittersPoint_Rock.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+//---------------------------------------------------------------------------
+//! @file   PoittersPoint_Rock.h
+//! @brief  PoittersPoint_Rock
+//---------------------------------------------------------------------------
+#include <System/Scene.h>
+
+namespace PoittersPoint {
+// namespace PoittersPoint
+
+USING_PTR(Rock);
+class Rock : public Object
+{
+public:
+    BP_OBJECT_DECL(Rock, "PoittersPoint::Rock");
+
+    //! @brief 初期化
+    //! @return 初期化終了
+    bool Init() override;
+
+    //! @brief 更新
+    //! キーを確認して弾を撃たせるために必要
+    void Update() override;
+
+    //! @brief GUI
+    //! GUIでの弾の速度調整のために必要
+    void GUI() override;
+
+    //! @brief 当たり判定のコールバック
+    //! 当たり判定が行われたときに自動で呼び出される関数
+    void OnHit(const ComponentCollision::HitInfo& hit_info) override;
+
+private:
+
+};
+
+}    // namespace PoittersPoint
+
+CEREAL_REGISTER_TYPE(PoittersPoint::Rock);
+CEREAL_REGISTER_POLYMORPHIC_RELATION(Object, PoittersPoint::Rock);
