db92adb5620980dd14e770dcbbf96c5bfbb0449d
502-25\cre <a>
2026-07-29 12:26:50 +0900

    [WIP]スライムのモデルの追加

---
+++ BaseProject2026_0613/src/Game/Object/PoittersPoint_Slime.cpp
@@ -0,0 +1,89 @@
+﻿//---------------------------------------------------------------------------
+//! @file   PoittersPoint_Slime.cpp
+//! @brief  PoittersPoint_Slime
+//---------------------------------------------------------------------------
+#include "PoittersPoint_Slime.h"
+#include "Game/Scene/PoittersPoint_Stage.h"
+
+#include <System/Scene.h>
+#include <System/Component/ComponentModel.h>
+#include "Game/Component/ComponentGrabbable.h"
+
+namespace PoittersPoint {
+// namespace PoittersPoint
+
+bool Slime::Init()
+{
+    // 親(継承元の基底クラス)のInit関数を呼ぶ
+    // これがなければabort()が呼ばれる
+    Super::Init();
+    //__super::Init();
+
+    SetTranslate({10, 10, 10});
+
+    SetName("Slime");
+    AddComponent<ComponentModel>("data/Game/Models/model.obj");
+
+    auto col = AddComponent<ComponentCollisionSphere>();
+
+    col->SetRadius(5.0f)->UseGravity(true);
+    col->SetGravity(-0.2f);
+    col->SetCollisionGroup(ComponentCollision::CollisionGroup::ETC);
+    col->SetHitCollisionGroup((u32)ComponentCollision::CollisionGroup::ENEMY | (u32)ComponentCollision::CollisionGroup::GROUND |
+                              (u32)ComponentCollision::CollisionGroup::ITEM | (u32)ComponentCollision::CollisionGroup::PLAYER);
+    col->SetMass(500.0f);
+
+    auto grabbable = AddComponent<ComponentGrabbable>();
+    grabbable->SetBounceOffset(0.2f);
+    grabbable->SetLiftTime(1.0f);
+
+    //AddComponent<ComponentCollisionModel>();
+    //if(auto collision = GetComponent<ComponentCollisionModel>()) {
+    //    collision->AttachToModel();    // コリジョンをモデルに合わせる
+    //}
+
+    SetScaleAxisXYZ({1.0f});
+
+    return true;
+}
+
+void Slime::Update()
+{
+    __super::Update();
+}
+
+void Slime::GUI()
+{
+    __super::GUI();
+}
+
+void Slime::OnHit(const ComponentCollision::HitInfo& hit_info)
+{
+    /*
+    // 当たった相手の名前がEnemyだったら消去する
+    auto name = hit_info.hit_collision_->GetOwner()->GetNameDefault();
+    if(name == "Enemy") {
+        hit_info.collision_->SetCollisionStatus(ComponentCollision::CollisionBit::DisableHit, true);
+        // 自分を削除する
+        Scene::Object::Release(SharedThis());
+    }
+    */
+    if(hit_info.hit_collision_->GetCollisionGroup() == ComponentCollision::CollisionGroup::GROUND) {
+        if(auto grabbable = GetComponent<ComponentGrabbable>()) {
+            float3 translation = grabbable->GetTranslation();
+            grabbable->SetCanGrab(true);
+
+            if(grabbable->IsGrounded()) {
+                grabbable->SetBounceOffset(0.2f);
+            }
+            else if(grabbable->IsMoving()) {
+                grabbable->Bounce();
+            }
+        }
+    }
+
+    // 最後にこれを入れてください。ここでめりこみの解消などの処理を行っています。
+    Super::OnHit(hit_info);
+}
+
+}    // namespace PoittersPoint
---
+++ BaseProject2026_0613/src/Game/Object/PoittersPoint_Slime.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+//---------------------------------------------------------------------------
+//! @file   PoittersPoint_Slime.h
+//! @brief  PoittersPoint_Slime
+//---------------------------------------------------------------------------
+#include <System/Scene.h>
+
+namespace PoittersPoint {
+// namespace PoittersPoint
+
+USING_PTR(Slime);
+class Slime : public Object
+{
+public:
+    BP_OBJECT_DECL(Slime, "PoittersPoint::Slime");
+
+    //! @brief 初期化
+    //! @return 初期化終了
+    bool Init() override;
+
+    //! @brief 更新
+    //! キーを確認して弾を撃たせるために必要
+    void Update() override;
+
+    //! @brief GUI
+    //! GUIでの弾の速度調整のために必要
+    void GUI() override;
+
+    //! @brief 当たり判定のコールバック
+    //! 当たり判定が行われたときに自動で呼び出される関数
+    void OnHit(const ComponentCollision::HitInfo& hit_info) override;
+
+private:
+};
+
+}    // namespace PoittersPoint
+
+CEREAL_REGISTER_TYPE(PoittersPoint::Slime);
+CEREAL_REGISTER_POLYMORPHIC_RELATION(Object, PoittersPoint::Slime);
